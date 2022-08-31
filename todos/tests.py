545342fbe2757b0582bf4d02bc53e6aab3f7979a
mohamed hesham <hesmammohammed@gmail.com>
2022-08-31 18:41:22 +0200

    list &  retrieve apis

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Todo
+
 
 # Create your tests here.
+class TODOModelsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="test title",
+            body="body for the test"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "test title")
+        self.assertEqual(str(self.todo), "test title")
+        self.assertEqual(self.todo.body, "body for the test")
